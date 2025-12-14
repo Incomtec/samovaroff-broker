e4b4c171dcbbbf66c92874f8f2b6d07a98a284d2
Dmitry Samovaroff <incomtec@gmail.com>
2025-12-14 18:02:35 +0300

    Day 0: finalization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use tracing::{debug, info};
+use tracing_subscriber::{EnvFilter, fmt};
+
+fn init_tracing() {
+    let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+
+    fmt()
+        .with_env_filter(filter)
+        .with_target(false)
+        .with_level(true)
+        .compact()
+        .init();
+}
+
 fn main() {
+    init_tracing();
+
+    info!("tracing starting");
+    debug!(pid = std::process::id(), "process info");
+
     println!("Hello, world!");
 }
